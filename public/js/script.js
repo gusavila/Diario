fbcaf6bc7c6148ac8a34f5ea6ddba2695d81682b
Gustavo Ávila <71139006+gusavila@users.noreply.github.com>
2025-02-03 21:16:55 -0300

    Enhance message input and interaction features
    Increased the height of the text area, added a background on hover,
    enabled message sending with 'Enter', and prevented sending
    empty messages.

---
+++ public/js/script.js
@@ -0,0 +1,20 @@
+function sendMensage() {
+    let textarea = document.querySelector("textarea");
+    let mensage = textarea.value.trim();
+
+    if (mensage !== "") {
+        document.querySelector("#message-form").submit();
+    }
+}
+
+document.querySelector("textarea").addEventListener("keydown", function(event) {
+    if (event.key === "Enter" && !event.shiftKey) {  
+        event.preventDefault();
+        sendMensage();
+    }
+});
+
+document.querySelector(".btn-enviar").addEventListener("click", function(event) {
+    event.preventDefault();
+    sendMensage(); 
+});
